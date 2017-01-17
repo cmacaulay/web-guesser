937466938ad4ff6d56e9521d0f262144a45f130e
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-01-16 18:12:59 -0700

    Extension: adds color depending on whether guess is too high or too low.

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,27 +2,32 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 SECRET_NUMBER = rand(100)
-
-
+@color = nil
 
 get '/' do
   guess   = params['guess'].to_i
   message = check_guess(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER,
-                          :message => message
+  erb :index, :locals => {:number  => SECRET_NUMBER,
+                          :message => message,
+                          :color   => @color
                          }
 end
 
 def check_guess(guess)
   if guess >= SECRET_NUMBER + 5
+    @color = "background: red;"
     "Way too high!"
   elsif guess > SECRET_NUMBER
+    @color = "background: fuchsia;"
     "Too high!"
   elsif guess <= SECRET_NUMBER - 5
+    @color = "background: red;"
     "Way too low!"
   elsif guess < SECRET_NUMBER
+    @color = "background: fuchsia;"
     "Too low!"
   else
+    @color = "background: green;"
     "You got it right! The secret number is #{SECRET_NUMBER}."
   end
 end
